1f83d6b675ae2a8615547e27b7863841573c4f39
svtvrn <nikoschasapis99@gmail.com>
2021-04-02 15:51:28 +0300

    part 7 done

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
+//Date object
 var date = new Date();
-var date_dom = (date.getUTCMonth()+1) +"-"+ date.getUTCDate() +"-"+ date.getUTCFullYear();
+//Adding date into the HTML files.
+var date_dom = (date.getUTCMonth()+1) +"-"+ date.getUTCDate() +"\n"+ date.getUTCFullYear();
 var weekday = date.toLocaleString('default', { weekday: 'short' });
 document.querySelector("#date").innerHTML =weekday+".\t"+ date_dom;
 weekdayHeaderTheme(date.getDay());
 
-
-
+//Night mode button and its event listener
 var nightModeButton = document.querySelector("#night-mode");
 nightModeButton.addEventListener('click',nightMode);
-
+//Light theme palette saved to change to original color scheme
 var style = getComputedStyle(document.body);
 var lightPalette = [ document.querySelector(".header").style.backgroundColor, document.querySelector(".footer").style.background, 
                     document.querySelector("body").style.backgroundColor, style.getPropertyValue('--clr--text-prim'),
                     style.getPropertyValue('--clr--text-side'), style.getPropertyValue('--clr--accent'),
                     style.getPropertyValue('--clr--accent-sec')];
 
+//Adding the rating buttons
+var likeButton = document.querySelector("#like");
+var dislikeButton = document.querySelector("#dislike");
+//Setting the event listeners
+likeButton.addEventListener('click',rateArticle);
+dislikeButton.addEventListener('click',rateArticle);
 
 
 
+//Night mode color switch function
 function nightMode(e){
     if(nightModeButton.value=="off"){
         nightModeButton.setAttribute('value','on');
@@ -40,6 +48,8 @@ function nightMode(e){
     }
 }
 
+
+//Header of the site changing colours based on the weekday number
 function weekdayHeaderTheme(day){
     switch(day){
         case 0:
@@ -66,5 +76,17 @@ function weekdayHeaderTheme(day){
         default:
             document.querySelector(".header").style.backgroundColor = "#662952";
             break;
+    }
+}
+
+function rateArticle(e){
+    if(e.target!==e.currentTarget){
+        if(e.currentTarget.id=="like"){
+            document.querySelector("#like-path").style.fill = "#1DD73C";
+            document.querySelector("#dislike-path").style.fill = "#66295270";
+        }else{
+            document.querySelector("#like-path").style.fill = "#66295270";
+            document.querySelector("#dislike-path").style.fill = "#C53041";
+        }
     }
 }
